bb7a41ab3e5fc4235fb26a24d4c25dacb3c859d2
MisterFoxyDev <misterfoxydev@gmail.com>
2024-06-07 16:17:30 +0200

    feat: :sparkles: Mise en place du système de rating sécurisé et de l'optimisation des images

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,4 +1,5 @@
 const Book = require("../models/Book");
+const fs = require("fs");
 
 //CREATE
 exports.createBook = (req, res) => {
@@ -18,12 +19,10 @@ exports.createBook = (req, res) => {
     .catch((err) => res.status(400).json({ err }));
 };
 
-// exports.createRating = req
 //READ
 exports.getOneBook = (req, res) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => {
-      console.log("🚀 ~ book:", book);
       return res.status(200).json(book);
     })
     .catch((err) => res.status(404).json({ err }));
@@ -33,17 +32,103 @@ exports.getAllBooks = (req, res) => {
     .then((books) => res.status(200).json(books))
     .catch((err) => res.status(404).json({ err }));
 };
+exports.getBestRating = (req, res) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => {
+      console.log("🚀 ~ books:", books)
+      return res.status(200).json(books)
+    })
+    .catch((err) => {
+      console.log("🚀 ~ err:", err)
+      return res.status(404).json({ err })
+    })
+};
 //UPDATE
 exports.modifyBook = (req, res) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Modification effectuée !" }))
+  const bookObject = req.file
+    ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  delete bookObject._userId;
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId !== req.auth.userId) {
+        res
+          .status(401)
+          .json({ message: "Vous n'êtes pas autorisé à modifier ce livre" });
+      } else {
+        Book.updateOne(
+          { _id: req.params.id },
+          { ...bookObject, _id: req.params.id },
+        )
+          .then(() => res.status(200).json({ message: "Livre modifié !" }))
+          .catch((err) => res.status(400).json({ err }));
+      }
+    })
     .catch((err) => res.status(400).json({ err }));
+};
+
+exports.addRating = (req, res) => {
+  const { userId, rating } = req.body;
+  if (rating < 0 || rating > 5) {
+    return res
+      .status(400)
+      .json({ message: "La note doit être comprise entre 0 et 5." });
+  }
+
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      const hasRated = book.ratings.some((r) => r.userId === userId);
+      if (hasRated) {
+        return res.status(400).json({
+          message: "Un utilisateur ne peut pas noter deux fois le même livre.",
+        });
+      }
+
+      const newRating = { userId, grade: rating };
+      const newRatings = [...book.ratings, newRating];
+
+      const total = newRatings.reduce((acc, curr) => acc + curr.grade, 0);
+      const averageRating = total / newRatings.length;
+
+      Book.updateOne(
+        { _id: req.params.id },
+        {
+          $push: { ratings: newRating },
+          $set: { averageRating: averageRating },
+        },
+      )
+        .then(() => {
+          book.ratings.push(newRating);
+          book.averageRating = averageRating;
+          res.status(201).json(book);
+        })
+        .catch((err) => res.status(400).json({ err }));
+    })
+    .catch((err) => res.status(404).json({ err }));
 };
 //DELETE
 exports.deleteBook = (req, res) => {
-  Book.deleteOne({
-    _id: req.params.id,
-  })
-    .then(() => res.status(200).json({ message: "Livre supprimé" }))
-    .catch((err) => res.status(400).json({ err }));
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
+        res
+          .status(401)
+          .json({ message: "Vous n'êtes pas autorisé à supprimer ce livre" });
+      } else {
+        const filename = book.imageUrl.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {
+          Book.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: "Livre supprimé !" }))
+            .catch((err) => res.status(401).json({ err }));
+        });
+      }
+    })
+    .catch((err) => res.status(500).json({ err }));
 };
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -5,13 +5,15 @@ const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
 //CREATE
-router.post("/", auth, multer, booksCtrl.createBook);
-router.post("/:id/rating", auth, )
+router.post("/", auth, multer.upload.single("image"), multer.resizeAndCompressImage, booksCtrl.createBook);
+
 //READ
 router.get("/", booksCtrl.getAllBooks);
+router.get("/bestrating", booksCtrl.getBestRating);
 router.get("/:id", booksCtrl.getOneBook);
 //UPDATE
-router.put("/:id", auth, booksCtrl.modifyBook);
+router.put("/:id", auth, multer.upload.single("image"), multer.resizeAndCompressImage, booksCtrl.modifyBook);
+router.post("/:id/rating", auth, booksCtrl.addRating);
 //DELETE
 router.delete("/:id", auth, booksCtrl.deleteBook);
 
